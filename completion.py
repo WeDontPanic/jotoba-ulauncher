a282d46e451e9b67979b81c51fbf34eaf4361f8f
jojii <jojii@gmx.net>
2022-10-01 17:51:39 +0200

    remove debug pritn

--- completion.py
+++ completion.py
@@ -8,6 +8,5 @@ def payload(query, search_type, lang):
 def request(payload):
     headers = {"Content-Type": "application/json; charset=utf-8", "Accept":"application/json"}
     data = json.dumps(payload, ensure_ascii=False).encode('utf-8')
-    print('payload',data)
     resp = requests.post(jotoba.COMPLETIONS, data = data, headers = headers)
     return json.loads(resp.text)
